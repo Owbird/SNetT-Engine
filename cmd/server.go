a46181efe32d3ad598fa8c794d3a3b4028f5862e
Owbird <owbirdphorccuo@gmail.com>
2025-10-23 01:00:43 +0000

    feat (server): Handle negative boolean options

--- cmd/server.go
+++ cmd/server.go
@@ -16,7 +16,6 @@ var (
 	notifConfig  = appConfig.GetNotifConfig()
 )
 
-// serverCmd represents the server command
 var serverCmd = &cobra.Command{
 	Use:   "server",
 	Short: "Manage file server",
@@ -26,7 +25,7 @@ var serverCmd = &cobra.Command{
 var startCmd = &cobra.Command{
 	Use:   "start",
 	Short: "Start the server",
-	Long:  `Start the server the server`,
+	Long:  `Start the file server with the given options.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		dir, err := cmd.Flags().GetString("dir")
 		if err != nil {
@@ -34,78 +33,52 @@ var startCmd = &cobra.Command{
 		}
 
 		logCh := make(chan models.ServerLog)
-
 		defer close(logCh)
 
-		wg := sync.WaitGroup{}
-
-		wg.Add(1)
 		go func() {
 			for l := range logCh {
 				switch l.Type {
 				case models.API_LOG:
-					if l.Type == models.SERVER_ERROR {
-						log.Printf("[!] API Error: %v", l.Value)
-					} else {
-						log.Printf("[+] API Log: %v", l.Value)
-					}
+					log.Printf("[+] API Log: %v", l.Value)
 				case models.SERVE_UI_LOCAL:
-					if l.Type == models.SERVER_ERROR {
-						log.Printf("[!] Network Web Run Error: %v", l.Value)
-					} else {
-						log.Printf("[+] Network Web Running: %v", l.Value)
-					}
+					log.Printf("[+] Network Web Running: %v", l.Value)
 				case models.SERVE_UI_REMOTE:
-					if l.Type == models.SERVER_ERROR {
-						log.Printf("[!] Remote Web Run Error: %v", l.Value)
-					} else {
-						log.Printf("[+] Remote Web Running: %v", l.Value)
-					}
-
+					log.Printf("[+] Remote Web Running: %v", l.Value)
+				case models.SERVER_ERROR:
+					log.Printf("[!] Server Error: %v", l.Value)
 				default:
-					if l.Type == models.SERVER_ERROR {
-						log.Printf("[!] Server Error: %v", l.Value)
-					} else {
-						log.Printf("[+] Server Log: %v", l.Value)
-					}
+					log.Printf("[+] Server Log: %v", l.Value)
 				}
 			}
 		}()
 
 		server := server.NewServer(dir, logCh)
 
-		allowUploads, err := cmd.Flags().GetBool("uploads")
-		if err != nil {
-			log.Fatalf("Failed to get 'uploads' flag: %v", err)
+		port, _ := cmd.Flags().GetInt("port")
+		serverName, _ := cmd.Flags().GetString("name")
+
+		if cmd.Flags().Changed("uploads") {
+			serverConfig.SetAllowUploads(true)
+		} else if cmd.Flags().Changed("no-uploads") {
+			serverConfig.SetAllowUploads(false)
 		}
 
-		allowOnline, err := cmd.Flags().GetBool("online")
-		if err != nil {
-			log.Fatalf("Failed to get 'online' flag: %v", err)
+		if cmd.Flags().Changed("online") {
+			serverConfig.SetAllowOnline(true)
+		} else if cmd.Flags().Changed("no-online") {
+			serverConfig.SetAllowOnline(false)
 		}
 
-		port, err := cmd.Flags().GetInt("port")
-		if err != nil {
-			log.Fatalf("Failed to get 'port' flag: %v", err)
+		if cmd.Flags().Changed("notify") {
+			notifConfig.SetAllowNotif(true)
+		} else if cmd.Flags().Changed("no-notify") {
+			notifConfig.SetAllowNotif(false)
 		}
 
-		serverName, err := cmd.Flags().GetString("name")
-		if err != nil {
-			log.Fatalf("Failed to get 'name' flag: %v", err)
-		}
-
-		allowNotif, err := cmd.Flags().GetBool("notify")
-		if err != nil {
-			log.Fatalf("Failed to get 'notify' flag: %v", err)
-		}
-
-		serverConfig.SetAllowUploads(allowUploads)
-		serverConfig.SetAllowOnline(allowOnline)
 		serverConfig.SetPort(port)
-
 		serverConfig.SetName(serverName)
 
-		notifConfig.SetAllowNotif(allowNotif)
+		wg := sync.WaitGroup{}
 
 		wg.Add(1)
 		go server.Start(*appConfig)
@@ -117,7 +90,7 @@ var startCmd = &cobra.Command{
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List available servers on the network",
-	Long:  `List reveals the broadcasted servers on the network for easy access`,
+	Long:  `List reveals the broadcasted servers on the network for easy access.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		server := server.NewServer("", nil)
 		servers := make(chan models.SNetTServer)
@@ -134,16 +107,23 @@ var listCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(serverCmd)
-
 	serverCmd.AddCommand(startCmd)
 	serverCmd.AddCommand(listCmd)
 
 	startCmd.Flags().StringP("dir", "d", "", "Directory to serve")
-	startCmd.Flags().StringP("name", "n", serverConfig.GetName(), "Directory to serve")
+	startCmd.Flags().StringP("name", "n", serverConfig.GetName(), "Server name")
 	startCmd.Flags().IntP("port", "p", serverConfig.GetPort(), "Port to host on")
+
 	startCmd.Flags().Bool("uploads", serverConfig.GetAllowUploads(), "Allow uploads to directory")
+	startCmd.Flags().Bool("no-uploads", !serverConfig.GetAllowUploads(), "Do not allow uploads to directory")
 	startCmd.Flags().Bool("online", serverConfig.GetAllowOnline(), "Allow online access to server")
+	startCmd.Flags().Bool("no-online", !serverConfig.GetAllowOnline(), "Do not allow online access to server")
 	startCmd.Flags().Bool("notify", notifConfig.GetAllowNotif(), "Allow notifications")
+	startCmd.Flags().Bool("no-notify", !notifConfig.GetAllowNotif(), "Do not allow notifications")
+
+	startCmd.MarkFlagsMutuallyExclusive("uploads", "no-uploads")
+	startCmd.MarkFlagsMutuallyExclusive("online", "no-online")
+	startCmd.MarkFlagsMutuallyExclusive("notify", "no-notify")
 
 	startCmd.MarkFlagRequired("dir")
 }
